498c796ae954482f3a81738bad9eca967f7c03f4
mojtabamoosaei <mojtabamoosaei6@gmail.com>
2024-05-30 01:36:05 +0330

    fix DescriptQuestion

---
+++ database/migrations/2024_05_29_211248_create_descriptivequestions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('descriptivequestions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('quize_id');
+            $table->foreign('quize_id')->references('id')->on('quizzes');
+            $table->char('type' , 255);
+            $table->text('title_questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('descriptivequestions');
+    }
+};
